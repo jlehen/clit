1988315bc33f7456f9328af404ab14e1a06f0920
Jeremie Le Hen <jeremie@le-hen.org>
2012-08-09 00:19:34 +0200

    [hiline] Make it possible to highlight the pattern only.
    Because on BSD and Solaris, awk(1) (or even nawk(1)) is not able to
    use backreferences in regex substitutions, use sed(1) instead.
    Incidentally, this make the script easier to read.

--- hiline.sh
+++ hiline.sh
@@ -5,7 +5,7 @@ usage() {
 Highlight lines matching patterns.
 OS: All.
 
-Usage: ${0##*/} [-p] <pattern> [ [color] [pattern [ [color] [...] ] ] ]
+Usage: ${0##*/} <pattern> [ [color] [pattern [ [color] [...] ] ] ]
 Patterns:
 	Awk regex (don't forget to escape '/').
 Colors:
@@ -15,22 +15,43 @@ Colors:
 	prefixed with "B".
 	If the first character is "=", only the pattern will be highlighed.
 	If no color is specified at all, then enbolden the line.
+Color examples:
 EOF
 	exit $1
 }
 
-case $(uname -s) in
-SunOS)
-	AWK=nawk
-	;;
-*)
-	AWK=awk
-	;;
-esac
+_esc="[";
+_fg=3; _bg=4;
+_esc256="8;5;";
+_m="m";
+_black=0;  _lblack=8;
+_red=1;    _lred=9;
+_green=2;  _lgreen=10;
+_yellow=3; _lyellow=11;
+_blue=4;   _lblue=12;
+_magenta=5;_lmagenta=13;
+_cyan=6;   _lcyan=14;
+_white=7;  _lwhite=15;
+_RESET="${_esc}0m";
+_BOLD="${_esc}1m";
+
+getcolor() {
+	local color="$1"
+	local fb="$2"
+
+	case "$color" in
+	*[abcdefghijklmnopqrstuvxyz]*) eval color=\"\$_$color\" ;;
+	esac
+	case "$color" in
+	'') return ;;
+	esac
+	eval fb=\"\$_$fb\"
+	echo "$_esc$fb$_esc256$color$_m"
+}
 
 [ $# -eq 0 -o "x$1" = "x-h" ] && usage 0
 
-awkscript=""
+sedscript=""
 while [ $# -gt 0 ]; do
 	pattern="$1"
 	color="$2"
@@ -38,75 +59,30 @@ while [ $# -gt 0 ]; do
 	[ $# -gt 0 ] && shift
 
 	case "$color" in
-	'='*) word=1; color=${color#=} ;;
-	*) word= ;;
+	'='*) w=; color=${color#=} ;;
+	*) w=".*" ;;
 	esac
 	case "$color" in
-	'B'*) bold=1; color=${color#B} ;;
+	'B'*) bold="$_BOLD"; color=${color#B} ;;
 	*) bold= ;;
 	esac
 
 	case "$color" in
-	*:*) fg=${color%:*}; bg=${color#*:} ;;
-	*) fg=$color; bg= ;;
+	*:*)
+		fg=$(getcolor "${color%:}" fg)
+		bg=$(getcolor "${color#*:}" bg)
+		;;
+	*)
+		fg=$(getcolor "${color}" fg)
+		bg=
+		;;
 	esac
-	[ "x$fg$bg" = "x" ] && bold=1
+	[ "x$fg$bg" = "x" ] && bold="$_BOLD"
 
-	awkscript="$awkscript
-/$pattern/ {
-	esc0=\"\";
-	esc1=_RESET;"
 
-	[ -n "$bold" ] && awkscript="$awkscript
-	esc0 = esc0 _BOLD;"
-
-	[ -n "$fg" ] && awkscript="$awkscript
-	esc0 = esc0 getcolor(\"$fg\", \"fg\");"
-
-	[ -n "$bg" ] && awkscript="$awkscript
-	esc0 = esc0 getcolor(\"$bg\", \"bg\");"
-
-	awkscript="$awkscript
-	printf(\"%s%s%s\n\", esc0, \$0, esc1);"
-
-	awkscript="$awkscript
-	next;
-}
-"
+	sedscript="$sedscript
+s/\\($w$pattern$w\\)/$bold$fg$bg\\1$_RESET/"
+		
 done
 
-exec $AWK '
-BEGIN {
-	_esc="[";
-	_fb["fg"]=3; _fb["bg"]=4
-	_esc256="8;5;";
-	_m="m";
-	_c["black"]=0;  _c["lblack"]=8;
-	_c["red"]=1;    _c["lred"]=9;
-	_c["green"]=2;  _c["lgreen"]=10;
-	_c["yellow"]=3; _c["lyellow"]=11;
-	_c["blue"]=4;   _c["lblue"]=12;
-	_c["magenta"]=5;_c["lmagenta"]=13;
-	_c["cyan"]=6;   _c["lcyan"]=14;
-	_c["white"]=7;  _c["lwhite"]=15;
-	_RESET=_esc"0m";
-	_BOLD=_esc"1m";
-}
-
-function getcolor(color, fb) {
-	if (color ~ /[a-z]/)
-		color  = _c[color];
-	if (color == "")
-		return;
-	return sprintf("%s", _esc _fb[fb] _esc256 color _m);
-}
-
-function reset() {
-	printf("%s", _RESET);
-}
-
-'"$awkscript"'
-{
-	print $0;
-}
-'
+sed "$sedscript"
